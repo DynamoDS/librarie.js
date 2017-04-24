219df0a11638205f14900339c343382c7c9fe748
fanwgwg <lahalaha90@gmail.com>
2017-04-24 15:45:26 +0800

    update image visibility when load fails

--- src/components/SearchResultItem.tsx
+++ src/components/SearchResultItem.tsx
@@ -25,7 +25,7 @@ export class SearchResultItem extends React.Component<SearchResultItemProps, Sea
 
         return (
             <div className={"SearchResultItemContainer"} onClick={this.onItemClicked.bind(this)}>
-                <img className={"ItemIcon"} src={iconPath} onError={this.onImageLoadFail.bind(this)} />
+                <img className={"ItemIcon"} src={iconPath} onLoad={this.onImageLoad.bind(this)} onError={this.onImageLoadFail.bind(this)} />
                 <div className={"ItemInfo"}>
                     <div className={"ItemTitle"}>{highLightedItemText}</div>
                     <div className={"ItemDetails"}>
@@ -35,6 +35,10 @@ export class SearchResultItem extends React.Component<SearchResultItemProps, Sea
                 </div>
             </div>
         );
+    }
+
+    onImageLoad(event: any) {
+        event.target.style.visibility = "visible";
     }
 
     onImageLoadFail(event: any) {
