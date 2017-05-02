54ee57d6418fdb9753bc34803566d0c8ab0286cc
ellensi <ellensi@live.com>
2017-05-02 23:08:24 +0800

    Revise category icons

--- src/components/SearchResultItem.tsx
+++ src/components/SearchResultItem.tsx
@@ -38,7 +38,7 @@ export class SearchResultItem extends React.Component<SearchResultItemProps, Sea
     }
 
     onImageLoadFail(event: any) {
-        event.target.src = require("../resources/ui/dynamo.png");
+        event.target.src = require("../resources/icons/Dynamo.svg");
     }
 
     onItemClicked() {
