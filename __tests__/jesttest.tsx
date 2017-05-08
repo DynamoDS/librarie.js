f3f4307fe5b2412a40dd3ffcf0c379ad21ac6536
fanwgwg <lahalaha90@gmail.com>
2017-05-08 10:31:14 +0800

    add jest

---
+++ __tests__/jesttest.tsx
@@ -0,0 +1,31 @@
+import Link from '../src/components/Link';
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+describe("", function() {
+
+});
+test("add two numbers", function() {
+    expect(1+2).toBe(3);
+});
+
+test('Link changes the class when hovered', () => {
+  const component = renderer.create(
+    <Link page="http://www.facebook.com">Facebook</Link>
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  
+  // // manually trigger the callback
+  // tree.props.onMouseEnter();
+  // // re-rendering
+  // tree = component.toJSON();
+  // expect(tree).toMatchSnapshot();
+
+  // // manually trigger the callback
+  // tree.props.onMouseLeave();
+  // // re-rendering
+  // tree = component.toJSON();
+  // expect(tree).toMatchSnapshot();
+});
+
