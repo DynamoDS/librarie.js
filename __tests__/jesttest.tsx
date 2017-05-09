8d0c2d53da9ac57068e89e9f692580976b12d8a8
fanwgwg <lahalaha90@gmail.com>
2017-05-09 15:34:15 +0800

    fix testcase

--- __tests__/jesttest.tsx
+++ __tests__/jesttest.tsx
@@ -1,81 +1,92 @@
 import * as React from 'react';
-import * as renderer from 'react-test-renderer';
-import * as ReactTestUtils from 'react-dom/lib/ReactTestUtils';
+import * as TestUtils from 'react-dom/lib/ReactTestUtils';
+import {shallow} from 'enzyme';
+import {expect} from 'chai';
 import * as LibraryEntryPoint from '../src/entry-point';
 import LibraryContainer from '../src/components/LibraryContainer';
 import LibraryItem from '../src/components/LibraryItem';
 
-var fs = require('fs');
-
-test("add two numbers", function () {
-  expect(1 + 2).toBe(3);
+describe("sample test", function () {
+  it("should add two numbers", function () {
+    expect(1 + 2).to.equal(3);
+  });
 });
 
-test("create library item", function () {
+describe("LibraryContainer", function () {
+  let loadedTypesJson: any;
+  let layoutSpecsJson: any;
+  let libController: LibraryEntryPoint.LibraryController;
 
-  let loadedTypesJson = {
-    "loadedTypes": [{
-      "fullyQualifiedName": "Child1",
-      "iconUrl": "",
-      "contextData": "Input",
-      "itemType": "action",
-      "keywords": ""
-    },
-    {
-      "fullyQualifiedName": "Child2",
-      "iconUrl": "",
-      "contextData": "",
-      "itemType": "action",
-      "keywords": ""
-    }
-    ]
-  };
-
-  let layoutSpecsJson = {
-    "sections": [{
-      "text": "default",
-      "iconUrl": "",
-      "elementType": "section",
-      "showHeader": false,
-      "include": [],
-      "childElements": [{
-        "text": "Parent",
+  beforeEach(function () {
+    loadedTypesJson = {
+      "loadedTypes": [{
+        "fullyQualifiedName": "Child1",
         "iconUrl": "",
-        "elementType": "category",
-        "include": [{
-          "path": "Child1"
-        },
-        {
-          "path": "Child2"
-        }
-        ],
+        "contextData": "Input",
+        "itemType": "action",
+        "keywords": ""
+      },
+      {
+        "fullyQualifiedName": "Child2",
+        "iconUrl": "",
+        "contextData": "",
+        "itemType": "action",
+        "keywords": ""
+      }
+      ]
+    };
+
+    layoutSpecsJson = {
+      "sections": [{
+        "text": "default",
+        "iconUrl": "",
+        "elementType": "section",
+        "showHeader": false,
+        "include": [],
+        "childElements": [{
+          "text": "Parent",
+          "iconUrl": "",
+          "elementType": "category",
+          "include": [{
+            "path": "Child1"
+          },
+          {
+            "path": "Child2"
+          }
+          ],
+          "childElements": []
+        }]
+      },
+      {
+        "text": "Miscellaneous",
+        "iconUrl": "",
+        "elementType": "section",
+        "showHeader": true,
+        "include": [],
         "childElements": []
       }]
-    },
-    {
-      "text": "Miscellaneous",
-      "iconUrl": "",
-      "elementType": "section",
-      "showHeader": true,
-      "include": [],
-      "childElements": []
-    }]
-  };
+    };
 
-  let libController = LibraryEntryPoint.CreateLibraryController();
+    libController = LibraryEntryPoint.CreateLibraryController();
 
-  libController.on("itemClicked", function (item) {
-    console.log(item);
-  })
+    libController.on("itemClicked", function (item) {
+      console.log(item);
+    })
 
-  libController.on("searchTextUpdated", function (searchText) {
-    console.log(searchText);
-    return null;
+    libController.on("searchTextUpdated", function (searchText) {
+      console.log(searchText);
+      return null;
+    });
+
   });
 
-  let libContainer = ReactTestUtils.renderIntoDocument(
-    libController.createLibraryContainer(layoutSpecsJson, loadedTypesJson)
-  );
+  it("should create a LibraryContainer", function () {
+    let libContainer = shallow(
+      libController.createLibraryContainer(layoutSpecsJson, loadedTypesJson)
+    );
+
+    expect(libContainer.find('SearchView')).to.have.lengthOf(1);
+  });
 });
 
 
