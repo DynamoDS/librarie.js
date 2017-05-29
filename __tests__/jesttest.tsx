8a060575ffc0a110ab9d27552d4eed39be8f60fd
monikaprabhu <monika.prabhu@autodesk.com>
2017-05-29 14:33:19 +0800

    Removed describe from the test as its not required for each test

--- __tests__/jesttest.tsx
+++ __tests__/jesttest.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import * as TestUtils from 'react-dom/lib/ReactTestUtils';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import { mount } from 'enzyme';
-import {expect} from 'chai';
+import { expect } from 'chai';
 import * as LibraryEntryPoint from '../src/entry-point';
-import { LibraryContainer }  from '../src/components/LibraryContainer';
+import { LibraryContainer } from '../src/components/LibraryContainer';
 import { LibraryItem } from '../src/components/LibraryItem';
 import { ItemData } from "../src/LibraryUtilities";
 
@@ -94,43 +94,40 @@ describe("LibraryContainer", function () {
     expect(libraryItem.prop('data').childItems[0].childItems[0].text).to.equal("Child1");
     expect(libraryItem.prop('data').childItems[0].childItems[1].text).to.equal("Child2");
   });
-   describe("Test mouse click event and change in expand state", function () {
-        it("Must recognise mouse event and change state", function () {
-          // test data to create LibraryItem  
-          // parent item that will hold the child items
-          let data = new ItemData("");
-          data.text = "TestItem";
-          data.itemType = "none";
-         
-          // create child items and link with parent
-          for (var i = 0; i < 2; i++) {
-            let item = new ItemData("");
-            item.text = "Child" + i;
-            item.itemType = "category";
-            data.appendChild(item);
-          }
+  it("Must recognise mouse event and change state", function () {
+    // test data to create LibraryItem  
+    // parent item that will hold the child items
+    let data = new ItemData("");
+    data.text = "TestItem";
+    data.itemType = "none";
 
-          // create "LibraryContainer" to pass as an argument for creation of "LibraryItem"
-          let libContainer = LibraryEntryPoint.CreateLibraryController();
-         
+    // create child items and link with parent
+    for (var i = 0; i < 2; i++) {
+      let item = new ItemData("");
+      item.text = "Child" + i;
+      item.itemType = "category";
+      data.appendChild(item);
+    }
 
-          // Mount is "real" rendering that will actually render your component into a browser environment. 
-          // If you are testing full React components, 
-          // mount is used to do rendering  and test actions are simulated on mounted html
-  
-          let libraryItem = mount(<LibraryItem libraryContainer={libContainer} data={data} />);
+    // create "LibraryContainer" to pass as an argument for creation of "LibraryItem"
+    let libContainer = LibraryEntryPoint.CreateLibraryController();
 
-          expect(libraryItem).to.have.lengthOf(1);
-          expect(libraryItem.props().data.childItems).to.have.lengthOf(2);
-          expect(libraryItem.props().data.text).to.equal("TestItem");
-          expect(libraryItem.props().data.showHeader).to.be.true;
-          expect(libraryItem.props().data.childItems[0].text).to.equal("Child0");
-          expect(libraryItem.props().data.childItems[1].text).to.equal("Child1");
-          let header = libraryItem.find(('div.LibraryItemHeader')).at(0);// the state of LibraryItem is changed when clicking on header
-          expect(header).to.have.lengthOf(1); // verify that there is a header
-          header.simulate('click'); // enzyme function to simulate mouse click
-          expect(libraryItem.state('expanded')).to.be.true; // check for if the libraryItem is expanded after mouse click       
-        });
+
+    // Mount is "real" rendering that will actually render your component into a browser environment. 
+    // If you are testing full React components, 
+    // mount is used to do rendering  and test actions are simulated on mounted html
+
+    let libraryItem = mount(<LibraryItem libraryContainer={libContainer} data={data} />);
+
+    expect(libraryItem).to.have.lengthOf(1);
+    expect(libraryItem.props().data.childItems).to.have.lengthOf(2);
+    expect(libraryItem.props().data.text).to.equal("TestItem");
+    expect(libraryItem.props().data.showHeader).to.be.true;
+    expect(libraryItem.props().data.childItems[0].text).to.equal("Child0");
+    expect(libraryItem.props().data.childItems[1].text).to.equal("Child1");
+    let header = libraryItem.find(('div.LibraryItemHeader')).at(0);// the state of LibraryItem is changed when clicking on header
+    expect(header).to.have.lengthOf(1); // verify that there is a header
+    header.simulate('click'); // enzyme function to simulate mouse click
+    expect(libraryItem.state('expanded')).to.be.true; // check for if the libraryItem is expanded after mouse click       
+  });
 });
-
-
