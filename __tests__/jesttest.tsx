ac28634bf4dd3aa7dc7ff1675672b7572bd1684c
monikaprabhu <monika.prabhu@autodesk.com>
2017-05-31 15:41:16 +0800

    Add comments to following files

--- __tests__/jesttest.tsx
+++ __tests__/jesttest.tsx
@@ -6,6 +6,8 @@ import * as LibraryEntryPoint from '../src/entry-point';
 import LibraryContainer from '../src/components/LibraryContainer';
 import LibraryItem from '../src/components/LibraryItem';
 
+// To run jest tests without coverage report use command "npm run jest"
+
 describe("sample test", function () {
   it("should add two numbers", function () {
     expect(1 + 2).to.equal(3);
