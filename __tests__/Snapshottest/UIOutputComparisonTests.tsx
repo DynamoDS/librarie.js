26a1bb5f8c8354ebc0532fae073fd2be115d4541
monikaprabhu <monika.prabhu@autodesk.com>
2017-06-15 11:15:54 +0800

    Miscellaneous fix as per PR comments - https://github.com/DynamoDS/librarie.js/pull/89

--- __tests__/Snapshottest/UIOutputComparisonTests.tsx
+++ __tests__/Snapshottest/UIOutputComparisonTests.tsx
@@ -1,28 +1,23 @@
 import * as React from 'react';
-//import * as TestUtils from 'react-dom/lib/ReactTestUtils';
 import * as LibraryEntryPoint from '../../src/entry-point';
 import { LibraryContainer } from '../../src/components/LibraryContainer';
 import { LibraryItem } from '../../src/components/LibraryItem';
 import { ItemData } from "../../src/LibraryUtilities";
-import * as renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
-
+import * as chai from 'chai';
 
 describe("LibraryContainer", function () {
-  let loadedTypesJson: any;
-  let layoutSpecsJson: any;
-  let libController: LibraryEntryPoint.LibraryController;
 
-  it("Test UI rendering of single component Library Item", function () {
-    // test data to create LibraryItem  
-    // parent item that will hold the child items
+  it("Test UI rendering of single component of Library Item", function () {
+    // Test data to create LibraryItem  
+    // Parent item that will hold the child items
     let data = new ItemData("");
     data.text = "TestItem";
     data.itemType = "none";
 
-    // create child items and link with parent
+    // Create child items and link with parent
     for (var i = 0; i < 2; i++) {
       let item = new ItemData("");
       item.text = "Child" + i;
@@ -30,17 +25,17 @@ describe("LibraryContainer", function () {
       data.appendChild(item);
     }
 
-    // create "LibraryContainer" to pass as an argument for creation of "LibraryItem"
+    // Create "LibraryContainer" to pass as an argument for creation of "LibraryItem"
     let libContainer = LibraryEntryPoint.CreateLibraryController();
     let libraryItem = shallow(<LibraryItem libraryContainer={libContainer} data={data} />);
     // When running for the first time creates a snapshot in __snapshots__ folder
     // To compare with the existing snapshot for subsequent running. 
-    expect(toJson(libraryItem)).toMatchSnapshot();  
+    expect(toJson(libraryItem)).toMatchSnapshot();
   });
 
   it("Test UI rendering of Library Item and child components", function () {
-    // test data to create LibraryItem  
-    // parent item that will hold the child items
+    // Test data to create LibraryItem  
+    // Parent item that will hold the child items
     let data = new ItemData("");
     data.text = "TestItem";
     data.itemType = "none";
@@ -52,12 +47,11 @@ describe("LibraryContainer", function () {
       item.itemType = "category";
       data.appendChild(item);
     }
-
-    // create "LibraryContainer" to pass as an argument for creation of "LibraryItem"
+    // Create "LibraryContainer" to pass as an argument for creation of "LibraryItem"
     let libContainer = LibraryEntryPoint.CreateLibraryController();
-    //Render wiht mount to test child components
+    // Render with mount to test child components
     let libraryItem = mount(<LibraryItem libraryContainer={libContainer} data={data} />);
-    expect(toJson(libraryItem)).toMatchSnapshot();  // toJson serializes the output
+    expect(toJson(libraryItem)).toMatchSnapshot();  // toJson serializes the output    
   });
 });
 
