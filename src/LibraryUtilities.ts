1e9c540935f49bc746eb3ebf2b28709560a74f9f
reddyashish <43763136+reddyashish@users.noreply.github.com>
2022-02-02 03:40:03 +0530

    Update LibraryUtilities.ts

--- src/LibraryUtilities.ts
+++ src/LibraryUtilities.ts
@@ -562,8 +562,7 @@ export function removeEmptyNodes(items: ItemData[]) {
             if (removeEmptyNodes(item.childItems)) {
                 i--;
             }
-        } 
-		// Do not remove the Add-ons section even when it has no child elements, as we want to show this section all times.
+        }// Do not remove the Add-ons section even when it has no child elements, as we want to show this section all times.
 		else if ( (item.itemType === "section" && item.contextData !== "Add-ons") || item.itemType === "category" || item.itemType === "group" || item.itemType === "coregroup") {
             items.splice(i, 1);
             i--;
