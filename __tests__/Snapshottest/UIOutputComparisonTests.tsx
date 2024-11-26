664ed7cb4b8eca9281334c91522ee4871ab26da2
enzo707 <enzo.batista@autodesk.com>
2024-11-26 17:29:41 -0300

    fix snapshot tests

--- __tests__/Snapshottest/UIOutputComparisonTests.tsx
+++ __tests__/Snapshottest/UIOutputComparisonTests.tsx
@@ -3,15 +3,12 @@
  */
 
 import * as React from 'react';
+import '@testing-library/jest-dom';
+import { prettyDOM, render } from '@testing-library/react';
 import * as LibraryEntryPoint from '../../src/entry-point';
 import { LibraryItem } from '../../src/components/LibraryItem';
 import { ItemData } from "../../src/LibraryUtilities";
 import { createLibraryItem } from "../../src/utils";
-import { mount, shallow, configure } from 'enzyme';
-import toJson from 'enzyme-to-json';
-import * as Adapter from 'enzyme-adapter-react-16';
-
-configure({adapter: new Adapter()});
 
 describe("LibraryContainer", function () {
   // Test data to create LibraryItem  
@@ -20,36 +17,36 @@ describe("LibraryContainer", function () {
 
   // Create "LibraryContainer" to pass as an argument for creation of "LibraryItem"
   let libcontroller = LibraryEntryPoint.CreateLibraryController()
-  let libContainer = mount(libcontroller.createLibraryContainer())
+  let libContainer = render(libcontroller.createLibraryContainer())
 
   // Creation of LibraryItem component 
   const libraryItemComponent = <LibraryItem libraryContainer={libContainer as any} data={data} showItemSummary={false} />;
 
   it("Test UI rendering of single component of Library Item", function () {
-    // "LibraryItem" with Shallow rendering to testing component as a unit.
-    let libraryItem = shallow(libraryItemComponent);
+    // "LibraryItem" with render() to testing component as a unit.
+    const {container} = render(libraryItemComponent);
 
     // When running for the first time creates a snapshot in __snapshots__ folder
     // To compare with the existing snapshot for subsequent running. 
-    expect(toJson(libraryItem)).toMatchSnapshot();
+    expect(prettyDOM(container)).toMatchSnapshot();
   });
 
   it("Test UI rendering of Library Item and child components", function () {
-    // Render with mount to test child components
-    let libraryItem = mount(libraryItemComponent);
-    expect(toJson(libraryItem)).toMatchSnapshot();  // toJson serializes the output    
+    // Render with render() to test child components
+    const { container } = render(libraryItemComponent);
+    expect(prettyDOM(container)).toMatchSnapshot(); 
   });
 
   it("Demonstrate testing UIitems loads correctly from static data", function () {
     // This is demo test to show how to test the LibrayUI is rendered correctly
     // Dynamo dynamcally generates the data in the libraryUI
     // So this test does not ensure all the required items are loaded there
-    let layoutSpecsJson = require("../../docs/LayoutSpecs.json");
-    let loadedTypesJson = require("../../docs/RawTypeData.json");
-    let libController = LibraryEntryPoint.CreateLibraryController();
+    const layoutSpecsJson = require("../../docs/LayoutSpecs.json");
+    const loadedTypesJson = require("../../docs/RawTypeData.json");
+    const libController = LibraryEntryPoint.CreateLibraryController();
     
-    // Render with mount to test child components
-    const tree =mount(libController.createLibraryContainer()); 
+    // Render with render() to test child components
+    const { container } = render(libController.createLibraryContainer()); 
     
     // required to dump the html version of librarie.js
     // create the library Controleer with the static data in the libraryui project 
@@ -58,8 +55,8 @@ describe("LibraryContainer", function () {
     libController.refreshLibraryView();
     
     // Search for all LibraryItems      
-    let text = tree.find('div.LibraryItemText');
-    expect(toJson(text)).toMatchSnapshot();
+    let text = container.querySelectorAll('div.LibraryItemText')[0];
+    expect(prettyDOM(text)).toMatchSnapshot();
   });
 });
 
